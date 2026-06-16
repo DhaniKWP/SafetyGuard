40bbc3e745b639674fa8ad7e3eb4987af7d275c2
dhanikwp <dhanikusuma256@gmail.com>
2026-06-16 14:53:54 +0700

    implement KaryawanDAO dengan CRUD dan juga penggun

--- src/dao/KaryawanDAO.java
+++ src/dao/KaryawanDAO.java
@@ -4,10 +4,63 @@
  */
 package dao;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import helper.SupabaseClient;
+import java.lang.reflect.Type;
+import java.util.List;
+import model.Karyawan;
 /**
  *
  * @author macbook
  */
 public class KaryawanDAO {
-    
+    private final Gson gson = new Gson();
+    // READ (Get All Karyawan)
+    public List<Karyawan> getAll() {
+        try {
+            String jsonResponse = SupabaseClient.get("/karyawan");
+            Type listType = new TypeToken<List<Karyawan>>() {}.getType();
+            return gson.fromJson(jsonResponse, listType);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    // CREATE (Insert Karyawan Baru)
+    public boolean insert(Karyawan karyawan) {
+        try {
+            karyawan.setIdKaryawan(null);
+            
+            String jsonPayload = gson.toJson(karyawan);
+            return SupabaseClient.post("/karyawan", jsonPayload);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    // UPDATE (Edit Karyawan berdasarkan ID)
+    public boolean update(Karyawan karyawan) {
+        try {
+            String id = karyawan.getIdKaryawan();
+            
+            String jsonPayload = gson.toJson(karyawan);
+            
+            String endpoint = "/karyawan?id_karyawan=eq." + id;
+            return SupabaseClient.patch(endpoint, jsonPayload);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    // DELETE (Hapus Karyawan berdasarkan ID)
+    public boolean delete(String id) {
+        try {
+            String endpoint = "/karyawan?id_karyawan=eq." + id;
+            return SupabaseClient.delete(endpoint);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
--- src/dao/PenggunaDAO.java
+++ src/dao/PenggunaDAO.java
@@ -4,10 +4,38 @@
  */
 package dao;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import helper.PasswordHasher;
+import helper.SupabaseClient;
+import java.lang.reflect.Type;
+import java.util.List;
+import model.Pengguna;
 /**
  *
  * @author macbook
  */
 public class PenggunaDAO {
-    
+    private final Gson gson = new Gson();
+
+    public Pengguna login(String username, String password) {
+        try {
+            String endpoint = "/pengguna?username=eq." + username;
+            String jsonResponse = SupabaseClient.get(endpoint);
+            Type listType = new TypeToken<List<Pengguna>>() {}.getType();
+            List<Pengguna> list = gson.fromJson(jsonResponse, listType);
+            if (list != null && !list.isEmpty()) {
+                Pengguna user = list.get(0);
+                
+                String hashedInput = PasswordHasher.hashPassword(password);
+                
+                if (user.getPassword().equals(hashedInput)) {
+                    return user;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
